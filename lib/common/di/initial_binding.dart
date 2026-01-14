6ef32d9b22856d6b2f9075c34ea616e04544fe89
Luiz Henrique Firmino de Jesus <luizhfjesus@gmail.com>
2026-01-13 22:37:54 -0300

    feat(setup): configure dependency injection with GetX

---
+++ lib/common/di/initial_binding.dart
@@ -0,0 +1,8 @@
+import 'package:get/get_instance/src/bindings_interface.dart';
+
+class InitialBinding implements Bindings {
+  @override
+  Future<void> dependencies() async {
+    // Add your dependencies here
+  }
+}
