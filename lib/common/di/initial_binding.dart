f66778d2f3f41676646386e0623b2c7a62e4b65c
Luiz Henrique Firmino de Jesus <luizhfjesus@gmail.com>
2026-01-19 14:00:25 -0300

    feat(data): implement isar service

--- lib/common/di/initial_binding.dart
+++ lib/common/di/initial_binding.dart
@@ -1,8 +1,10 @@
 import 'package:get/get.dart';
+import 'package:sticker_manager_wc22/common/data/services/isar_service.dart';
 
 class InitialBinding implements Bindings {
   @override
   Future<void> dependencies() async {
-    // register core services here.
+    await Get.putAsync<IsarService>(() => IsarService().init());
+
   }
 }
