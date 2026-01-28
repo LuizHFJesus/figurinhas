53f4746e90f78482e25c5f06ed85ef5de583ebef
Luiz Henrique Firmino de Jesus <luizhfjesus@gmail.com>
2026-01-28 00:55:52 -0300

    feat(models): implement models

---
+++ lib/domain/models/section.dart
@@ -0,0 +1,17 @@
+class Section {
+  final String sectionId;
+  final String albumId;
+  final String groupId;
+  final String name;
+  final String icon;
+  final int order;
+
+  const Section({
+    required this.sectionId,
+    required this.albumId,
+    required this.groupId,
+    required this.name,
+    required this.icon,
+    required this.order,
+  });
+}
