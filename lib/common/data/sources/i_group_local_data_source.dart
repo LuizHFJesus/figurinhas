98948913024de3152bf86fcd00a396ff6edb110d
Luiz Henrique Firmino de Jesus <luizhfjesus@gmail.com>
2026-01-30 14:11:42 -0300

    chore(project): deleting old files

--- lib/common/data/sources/i_group_local_data_source.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dart_either/dart_either.dart';
-import 'package:sticker_manager_wc22/common/domain/models/group.dart';
-import 'package:sticker_manager_wc22/common/utils/failure.dart';
-
-abstract class IGroupLocalDataSource {
-  Future<Either<Failure, void>> saveGroups(List<Group> groups);
-
-  Future<Either<Failure, List<Group>>> getGroupsByAlbum(String albumId);
-}
