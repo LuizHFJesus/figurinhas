f54d1d5b1f46c4f9e1fa47ea1b2f40901b11bf26
Luiz Henrique Firmino de Jesus <luizhfjesus@gmail.com>
2026-01-30 13:05:19 -0300

    feat(repositories): implement repository interfaces

---
+++ lib/domain/repositories/stats_repository.dart
@@ -0,0 +1,30 @@
+import 'package:sticker_manager_wc22/domain/models/album_stats.dart';
+import 'package:sticker_manager_wc22/domain/models/section_stats.dart';
+
+abstract class StatsRepository {
+  Future<AlbumStats> getAlbumStats(String userAlbumId);
+
+  Stream<AlbumStats> watchAlbumStats(String userAlbumId);
+
+  Future<SectionStats> getSectionStats({
+    required String userAlbumId,
+    required String sectionId,
+  });
+
+  Stream<SectionStats> watchSectionStats({
+    required String userAlbumId,
+    required String sectionId,
+  });
+
+  /// Internal bootstrap helpers (called by updater/usecases)
+  Future<void> ensureAlbumStatsInitialized({
+    required String userAlbumId,
+    required String albumId,
+  });
+
+  Future<void> ensureSectionStatsInitialized({
+    required String userAlbumId,
+    required String albumId,
+    required String sectionId,
+  });
+}
---
+++ lib/domain/repositories/user_album_repository.dart
@@ -0,0 +1,21 @@
+import 'package:sticker_manager_wc22/domain/models/user_album.dart';
+
+abstract class UserAlbumRepository {
+  Future<UserAlbum> createUserAlbum({
+    required String profileId,
+    required String albumId,
+    required String name,
+    bool setActive = true,
+  });
+
+  Future<List<UserAlbum>> listUserAlbums(String profileId);
+
+  Future<UserAlbum> getActiveUserAlbum(String profileId);
+
+  Stream<UserAlbum> watchActiveUserAlbum(String profileId);
+
+  Future<void> setActiveUserAlbum({
+    required String profileId,
+    required String userAlbumId,
+  });
+}
