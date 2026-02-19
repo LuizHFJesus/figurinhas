421060a05f6345290632c67df5c4ee950ddd6096
Luiz Henrique Firmino de Jesus <luizhfjesus@gmail.com>
2026-02-19 17:03:33 -0300

    feat(home): implement home action buttons

---
+++ lib/ui/common/widgets/action_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:sticker_manager_wc22/ui/common/widgets/svg_icon.dart';
+
+class ActionButton extends StatelessWidget {
+  final String icon;
+  final String label;
+  final VoidCallback onTap;
+
+  const ActionButton({
+    required this.icon,
+    required this.label,
+    required this.onTap,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final colorScheme = theme.colorScheme;
+    final textTheme = theme.textTheme;
+
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(12),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 18),
+        decoration: BoxDecoration(
+          color: colorScheme.onPrimary,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          children: [
+            SvgIcon(icon, color: colorScheme.onSurfaceVariant),
+            const SizedBox(height: 10),
+            Text(label, style: textTheme.labelMedium),
+          ],
+        ),
+      ),
+    );
+  }
+}
