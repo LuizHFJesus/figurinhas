93758b787b766f48e15583b2a990fc60d6942f36
Luiz Henrique Firmino de Jesus <luizhfjesus@gmail.com>
2026-03-03 14:47:35 -0300

    feat(ads): implement stats view banner ad

--- lib/ui/stats/controllers/stats_controller.dart
+++ lib/ui/stats/controllers/stats_controller.dart
@@ -1,25 +1,34 @@
 import 'package:get/get.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:sticker_manager_wc22/core/ads/ad_unit_ids.dart';
 import 'package:sticker_manager_wc22/domain/models/album_stats.dart';
 import 'package:sticker_manager_wc22/domain/models/user_album.dart';
 import 'package:sticker_manager_wc22/domain/repositories/user_profile_repository.dart';
 import 'package:sticker_manager_wc22/domain/usecases/get_active_user_album_usecase.dart';
 import 'package:sticker_manager_wc22/domain/usecases/watch_album_stats_usecase.dart';
+import 'package:sticker_manager_wc22/ui/ads/usecases/load_banner_ad_usecase.dart';
 import 'package:sticker_manager_wc22/ui/stats/models/stats_route_args.dart';
 
 class StatsController extends GetxController {
   final UserProfileRepository _profileRepo;
   final GetActiveUserAlbumUseCase _getActiveAlbum;
   final WatchAlbumStatsUseCase _watchAlbumStats;
+  final LoadBannerAdUseCase _loadBannerUseCase;
 
   final StatsRouteArgs? args;
 
   final Rx<UserAlbum?> album = Rx<UserAlbum?>(null);
   final Rx<AlbumStats?> stats = Rx<AlbumStats?>(null);
 
+  // Ad
+  RxBool get isBannerReady => _loadBannerUseCase.isBannerReady;
+  BannerAd? get bannerAd => _loadBannerUseCase.bannerAd;
+
   StatsController(
     this._profileRepo,
     this._getActiveAlbum,
-    this._watchAlbumStats, {
+    this._watchAlbumStats,
+    this._loadBannerUseCase, {
     required this.args,
   });
 
@@ -29,6 +38,12 @@ class StatsController extends GetxController {
     album.value = args?.album;
     stats.value = args?.stats;
     await _loadStats();
+  }
+
+  @override
+  void onClose() {
+    _loadBannerUseCase.dispose();
+    super.onClose();
   }
 
   Future<void> _loadStats() async {
@@ -44,5 +59,7 @@ class StatsController extends GetxController {
           albumId: currentAlbum.albumId,
         )
         .listen((s) => stats.value = s);
+
+    await _loadBannerUseCase.call(adUnitId: AdUnitIds.statsBanner);
   }
 }
