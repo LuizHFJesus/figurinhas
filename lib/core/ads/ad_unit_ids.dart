a7446866aa858c96369be39a02bc1308af6d8938
Luiz Henrique Firmino de Jesus <luizhfjesus@gmail.com>
2026-04-18 21:15:06 -0300

    refactor: update app ads configuration

--- lib/core/ads/ad_unit_ids.dart
+++ lib/core/ads/ad_unit_ids.dart
@@ -1,7 +1,7 @@
 class AdUnitIds {
   static const androidTestBanner = 'ca-app-pub-3940256099942544/6300978111';
-  static const homeBanner = 'ca-app-pub-9491568677895742/6335428030';
-  static const overviewBanner = 'ca-app-pub-9491568677895742/4511882929';
-  static const sectionBanner = 'ca-app-pub-9491568677895742/2328097357';
-  static const statsBanner = 'ca-app-pub-9491568677895742/2676057071';
+  static const homeBanner = 'ca-app-pub-9491568677895742/3266470977';
+  static const overviewBanner = 'ca-app-pub-9491568677895742/5181141691';
+  static const sectionBanner = 'ca-app-pub-9491568677895742/1953389308';
+  static const statsBanner = 'ca-app-pub-9491568677895742/3868060028';
 }
